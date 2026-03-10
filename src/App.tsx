5bdf500887573729967a4ba14fe9e74bdfa4e28b
gabrieljorda <gabrieleminhamae@hotmail.com>
2026-03-10 10:19:48 -0300

    Initial commit: React + Vite setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        meu controle financeiro 
+      </h1>
+    </div>
+  )
+}
